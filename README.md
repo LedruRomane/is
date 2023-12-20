0c9886c4c2ce8cb48113990d4189ba7b42d0ddfe
Emmanuel Coquery <emmanuel.coquery@univ-lyon1.fr>
2023-12-20 11:04:32 +0100

    init tp1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Codes source pour les TP TIW - Intergiciels et Services 2023-2024
---
+++ catalogue-vols/src/main/java/tiw/is/vols/catalogue/App.java
@@ -0,0 +1,13 @@
+package tiw.is.vols.catalogue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
