cc9c7e21fdfc93c694f5a90dfcf1c5f074ac0932
romane-ledru <romane.ledru@elao.com>
2024-01-01 14:27:14 +0100

    mend

--- catalogue-vols/src/main/java/tiw/is/vols/catalogue/db/PersistenceManager.java
+++ catalogue-vols/src/main/java/tiw/is/vols/catalogue/db/PersistenceManager.java
@@ -10,13 +10,21 @@ public class PersistenceManager {
 
     public static EntityManagerFactory createEntityManagerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put("hibernate.hbm2ddl.auto", "update");
-        config.put("jakarta.persistence.jdbc.url", "jdbc:postgresql:catalogue-db");
-        config.put("jakarta.persistence.jdbc.user", "catalogue");
-        config.put("jakarta.persistence.jdbc.password", "catalogue-mdp");
-        config.put("hibernate.hbm2ddl.auto", "create");
-        return Persistence
-                .createEntityManagerFactory("pu-catalogue", config);
-    }
+        String dbUrl = System.getenv("DB_URL");
+        String dbUser = System.getenv("POSTGRES_USER");
+        String dbPassword = System.getenv("POSTGRES_PASSWORD");
 
+        //print
+        System.out.println("DB_URL: " + dbUrl);
+        System.out.println("POSTGRES_DB: " + System.getenv("POSTGRES_DB"));
+        System.out.println("POSTGRES_USER: " + System.getenv("POSTGRES_USER"));
+
+        config.put("hibernate.hbm2ddl.auto", "update");
+        config.put("jakarta.persistence.jdbc.url", (dbUrl != null) ? dbUrl : "jdbc:postgresql:catalogue-db");
+        config.put("jakarta.persistence.jdbc.user", dbUser != null ? dbUser : "catalogue");
+        config.put("jakarta.persistence.jdbc.password", dbPassword != null ? dbPassword : "catalogue-mdp");
+        config.put("hibernate.hbm2ddl.auto", "create");
+        return Persistence.createEntityManagerFactory("pu-catalogue", config);
+
+    }
 }
